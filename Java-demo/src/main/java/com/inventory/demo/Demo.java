e6584257fcb5d6fd192d72b2418b60330591a490
Nikita <xoxlov.nikita.2005@gmail.com>
2025-12-11 05:21:13 +0300

    Файл для работы Maven

---
+++ Java-demo/src/main/java/com/inventory/demo/Demo.java
@@ -0,0 +1,8 @@
+package com.inventory.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Maven работает! Java-программа собрана успешно.");
+    }
+}
+// java -cp target/java-demo-1.0-SNAPSHOT.jar com.inventory.demo.Demo
